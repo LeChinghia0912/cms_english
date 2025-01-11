5833fc238108bca38734a3f170372954ff5025a6
unknown <nghia520414@gmail.com>
2025-01-11 13:02:13 +0700

    complete

--- src/yups/chapter.js
+++ src/yups/chapter.js
@@ -2,8 +2,7 @@ import { object, string } from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 const ChapterSchema = object({
-  name: string().required("Trường này không được để trống").matches(/^chương \d+$/, "Tên chương phải theo định dạng chương 1, với số là dynamic"),
-  title: string().required("Trường này không được để trống"),
+  name: string().required("Trường này không được để trống"),
   poster: string().required("Trường này không được để trống"),
 });
 
--- src/yups/lesson.js
+++ src/yups/lesson.js
@@ -2,7 +2,7 @@ import { object, string } from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 const LessonSchema = object({
-  name: string().required("Trường này không được để trống").matches(/^bài \d+$/, "Tên bài học phải theo định dạng bài 1, với số là dynamic"),
+  name: string().required("Trường này không được để trống"),
   title: string().required("Trường này không được để trống"),
   poster: string().required("Trường này không được để trống"),
 });
